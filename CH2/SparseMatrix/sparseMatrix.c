7450479ae0663f4ac85286ba4757e3f1f21d9b76
StoneBreaker <halfcentry@gmail.com>
2019-04-18 11:46:37 +0800

    temporary mmult function

--- CH2/SparseMatrix/sparseMatrix.c
+++ CH2/SparseMatrix/sparseMatrix.c
@@ -4,6 +4,17 @@
 
 #define MAX_TERMS	101
 #define MAX_COL		100
+#define COMPARE(a, b)\
+	({\
+		int returnValue = 0;\
+		if((a)>(b))\
+			returnValue = 1;\
+		else if((a)==(b))\
+			returnValue = 0;\
+		else\
+			returnValue = -1;\
+		returnValue;\
+	})
 
 //===================================================================
 //
@@ -190,5 +201,71 @@ void add(term a[], term b[], term c[])
 //===================================================================
 void mmult(term a[], term b[], term d[])
 {
+	/* multiply two sparse matrices */
+	int i, j, column, totalD = 0;
+	int rowsA = a[0].row, colsA = a[0].col, totalA = a[0].value;
+	int colsB = b[0].col, totalB = b[0].value;
+	int rowBegin = 1, row = a[1].row, sum = 0;
+	term newB[MAX_TERMS];
 	
+	if(colsA != b[0].row){
+		fprintf(stderr, "Incompatible matrices\n");
+		exit(EXIT_FAILURE);
+	}
+	fastTranspose(b, newB);
+	/* set boundary condition */
+	a[totalA+1].row = rowsA;
+	newB[totalB+1].row = colsB;
+	newB[totalB+1].col = 0;
+	
+	for(i = 1; i <= totalA;) {
+		column = newB[1].row;
+		for(j = 1; j <= totalB+1;) {
+			/* multiply row of a by column of b */
+			if(a[i].row != row){
+				storeSum(d, &totalD, row, column, &sum);
+				i = rowBegin;
+				for(; newB[j].row == column; ++j);
+				column = newB[j].row;
+			}
+			else if(newB[j].row != column){
+				storeSum(d, &totalD, row, column, &sum);
+				i = rowBegin;
+				column = newB[j].row;
+			}
+			else switch (COMPARE(a[i].col, newB[j].col)) {
+				case -1: /* go to next term in a */
+					i++; break;
+				case 0: /* add terms, go to next term in a and b */
+					sum += (a[i++].value*newB[j++].value);
+					break;
+				case 1: /* advance to next term in b */
+					j++;
+					break;
+				}
+		} /* end of for j <= totalB+1 */
+		for(; a[i].row == row; ++i);
+		rowBegin = i; row = a[i].row;
+	} /* end of for i <= totalA */
+}
+
+//===================================================================
+//
+//===================================================================
+void storeSum(term d[], int *totalD, int row, int column, int *sum)
+{
+	/* if *sum != 0, then it along with its row and column 
+		position is stored as the *totalD+1 entry in d*/
+	if(*sum)
+		if(*totalD < MAX_TERMS) {
+			d[++*totalD].row = row;
+			d[*totalD].col = column;
+			d[*totalD].value = *sum;
+			*sum = 0;
+		}
+		else {
+			fprintf(stderr, "Numbers of terms in product exceeds %d\n", MAX_TERMS);
+			  
+			exit(EXIT_FAILURE);
+		}
 }
--- CH2/SparseMatrix/sparseMatrix.h
+++ CH2/SparseMatrix/sparseMatrix.h
@@ -15,5 +15,6 @@ void	transpose(term a[], term b[]);
 void	fastTranspose(term a[], term b[]);
 void	add(term a[], term b[], term c[]);
 void	mmult(term a[], term b[], term d[]);
+void	storeSum(term d[], int *totalD, int row, int column, int *sum);
 
 #endif
